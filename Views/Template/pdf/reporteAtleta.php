38d6b4a0178b4b5ed8e2756d3775b0e0310b24a8
cristianlrj <cristianrojasjimenez2003@gmail.com>
2024-10-07 17:57:54 -0400

    changes stetic

--- Views/Template/pdf/reporteAtleta.php
+++ Views/Template/pdf/reporteAtleta.php
@@ -1,4 +1,32 @@
-<h1><?= $nombre ?></h1>
+<img src="<?= BASE_URL ?>/Assets/images/cintillo.jpg" alt="">
+
+<h2><?= $nombre ?></h2>
+
+<style>
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      table-layout: auto;
+    }
+
+    th, td {
+      padding: 8px;
+      text-align: left;
+    }
+
+    th {
+      background-color: #67000C; /* Color morado de Material Design */
+      color: white;
+      text-transform: uppercase;
+      font-size: 12px;
+    }
+
+    td {
+      border-bottom: 1px solid #e0e0e0;
+      color: #424242;
+      font-size: 12px;
+    }
+</style>
 
 <table style="width: 100%;">
     <thead>
@@ -7,7 +35,7 @@
                 for ($i=0; $i < count($campos); $i++) {
             ?>
 
-                <th style="text-align: left; padding-left: 2px"><?= ucfirst(str_replace("_", " ", $campos[$i])) ?></th>
+                <th style="text-align: left;"><?= ucfirst(str_replace("_", " ", $campos[$i])) ?></th>
                 
             <?php  
                 }
@@ -25,7 +53,7 @@
                     for ($a=0; $a < count($campos); $a++) {
                 ?>
 
-                    <td><?= $datos[$i][$campos[$a]] ?></td>
+                    <td style="text-align: left;"><?= $datos[$i][$campos[$a]] ?></td>
 
                 <?php  
                     }
